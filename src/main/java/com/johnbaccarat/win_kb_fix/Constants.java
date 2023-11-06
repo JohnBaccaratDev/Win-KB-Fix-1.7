ace8a50fafdb6a8f93cdff3b0b93e8371e392453
= <=>
2023-11-06 18:50:47 +0100

    Increase minor version

--- src/main/java/com/johnbaccarat/win_kb_fix/Constants.java
+++ src/main/java/com/johnbaccarat/win_kb_fix/Constants.java
@@ -6,7 +6,7 @@ public abstract class Constants {
 
 	public static final String MOD_ID = "win_kb_fix";
 	public static final String MOD_NAME = "Windows Keyboard Fixes";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static Logger LOG;
 
 
